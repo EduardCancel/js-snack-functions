fab0148e47933f366f7f1cafbd58fe8915f6c987
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-17 15:18:15 +0100

    complete second task

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function initialWord(arr){
+    const intial = []
+    for( let i = 0; i < arr.length; i++){
+        let thisName = arr[i];
+        intial.push(thisName[0]);
+    }
 
+    return intial
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(initialWord(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
