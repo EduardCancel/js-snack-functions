1b499867862c81bf83d1858e1ec58b893e887cc7
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-17 16:02:57 +0100

    complete fourth task

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,22 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function stringVocali(string){
+    let vowelsNumber = 0
+    let vowelsFound = [];
 
+    for (let i = 0; i < string.length; i++){
+        let thisVowels1 = string[i]
+        if (thisVowels1 === 'a' || thisVowels1 === 'e' || thisVowels1 === 'i' || thisVowels1 === 'o' || thisVowels1 === 'u'){
+            vowelsNumber ++;
+            vowelsFound.push(thisVowels1)
+        }
+    }
+    return vowelsNumber, vowelsFound
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(stringVocali(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
